e9fe244b1d99f1faa2e2ffb555c7e02e01a18219
MMatvei7 <112868094+MMatvei7@users.noreply.github.com>
2022-11-22 00:55:05 +0300

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -1,62 +1,86 @@
+import Globals
 import pygame
-import time
-import random
 import pygame_menu
+import random
+import time
 
+globals = Globals.Globals()
 # Window size
-window_x = 720
-window_y = 480
+globals.window_x = 720
+globals.window_y = 480
 
 # defining colors
-black = pygame.Color(0, 0, 0)
-white = pygame.Color(255, 255, 255)
-red = pygame.Color(255, 0, 0)
-green = pygame.Color(0, 255, 0)
-blue = pygame.Color(0, 0, 255)
-yellow = pygame.Color(255, 255, 0)
+globals.black = pygame.Color(0, 0, 0)
+globals.white = pygame.Color(255, 255, 255)
+globals.red = pygame.Color(255, 0, 0)
+globals.green = pygame.Color(0, 255, 0)
+globals.blue = pygame.Color(0, 0, 255)
+globals.yellow = pygame.Color(255, 255, 0)
 
 # Initialising pygame
 pygame.init()
 
 # Initialise game window
 pygame.display.set_caption('Snake game')
-game_window = pygame.display.set_mode((window_x, window_y))
+game_window = pygame.display.set_mode((globals.window_x, globals.window_y))
 
 # FPS (frames per second) controller
 fps = pygame.time.Clock()
-tmp = 15
+globals.tmp = 15
+
+globals.rating_score = [0] * 3
 
 
 # set game speed
 def set_difficulty(value, difficulty=15):
-    global tmp
-    tmp = difficulty
+    globals.tmp = difficulty
 
 
-color_ = green
+globals.color_ = globals.green
 
 
 # set snake color
-def set_color(value, color=green):
-    global color_
-    color_ = color
+def set_color(value, color=globals.green):
+    globals.color_ = color
 
 
-window_color = black
+globals.window_color = globals.black
 
 
 # set window color
-def set_window(value, color=black):
-    global window_color
-    window_color = color
+def set_window(value, color=globals.black):
+    globals.window_color = color
+
+
+def draw_text(k, y):
+    my_font = pygame.font.SysFont('Times', 20)
+    game_over_surface = my_font.render(str(-k) + ': ' + str(sorted(globals.rating_score)[k]),
+                                       True,
+                                       globals.red)
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (globals.window_x / 2, y)
+    game_window.blit(game_over_surface, game_over_rect)
+
+
+def show_rating():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                menu_r()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    menu_r()
+        game_window.fill(globals.window_color)
+        draw_text(-1, 100)
+        draw_text(-2, 200)
+        draw_text(-3, 300)
+        pygame.display.update()
 
 
 def start_the_game():
-    global tmp
-    snake_speed = tmp
+    snake_speed = globals.tmp
 
-    global window_color
-    game_window.fill(window_color)
+    game_window.fill(globals.window_color)
 
     # show rating table
     def show_score(choice, color, font, size, score):
@@ -69,35 +93,35 @@ def start_the_game():
     def game_over(score):
         my_font = pygame.font.SysFont('Times', 50)
         game_over_surface = my_font.render(
-            'Your Score is : ' + str(score), True, red)
+            'Your Score is : ' + str(score), True, globals.red)
         game_over_rect = game_over_surface.get_rect()
-        game_over_rect.midtop = (window_x / 2, window_y / 4)
+        game_over_rect.midtop = (globals.window_x / 2, globals.window_y / 4)
         game_window.blit(game_over_surface, game_over_rect)
         pygame.display.flip()
         time.sleep(1)
-        pygame.quit()
-        quit()
+        globals.rating_score.append(score)
+        menu_r()
 
-    # create first fruit type: pineapple - blue fruit
+    # create first fruit type: pineapple - globals.blue fruit
     class Pineapple:
         fruit_spawn = True
         count_pineapple = 0
 
         # initialization
         def __init__(self):
-            self.fruit_position = [random.randrange(1, ((window_x - 100) // 10)) * 10,
-                                   random.randrange(1, ((window_y - 100) // 10)) * 10]
+            self.fruit_position = [random.randrange(1, ((globals.window_x - 100) // 10)) * 10,
+                                   random.randrange(1, ((globals.window_y - 100) // 10)) * 10]
 
             self.fruit_spawn = True
-            self.fruit_color = blue
+            self.fruit_color = globals.blue
             self.count_pineapple = 1
 
         # create random pineapple position
         def fruit_appear(self):
-            self.fruit_position = [random.randrange(1, ((window_x - 100) // 10)) * 10,
-                                   random.randrange(1, ((window_y - 100) // 10)) * 10]
+            self.fruit_position = [random.randrange(1, ((globals.window_x - 100) // 10)) * 10,
+                                   random.randrange(1, ((globals.window_y - 100) // 10)) * 10]
 
-            self.fruit_color = blue
+            self.fruit_color = globals.blue
 
         # draw pineapple
         def fruit_window_appear(self):
@@ -108,22 +132,20 @@ def start_the_game():
         fruit_spawn = True
 
         def __init__(self):
-            self.fruit_position = [random.randrange(1, ((window_x - 100) // 10)) * 10,
-                                   random.randrange(1, ((window_y - 100) // 10)) * 10]
+            self.fruit_position = [random.randrange(1, ((globals.window_x - 100) // 10)) * 10,
+                                   random.randrange(1, ((globals.window_y - 100) // 10)) * 10]
 
             self.fruit_spawn = True
-            self.fruit_color = white
+            self.fruit_color = globals.white
 
         def fruit_appear(self):
-            self.fruit_position = [random.randrange(1, ((window_x - 100) // 10)) * 10,
-                                   random.randrange(1, ((window_y - 100) // 10)) * 10]
-            self.fruit_color = white
+            self.fruit_position = [random.randrange(1, ((globals.window_x - 100) // 10)) * 10,
+                                   random.randrange(1, ((globals.window_y - 100) // 10)) * 10]
+            self.fruit_color = globals.white
 
         def fruit_window_appear(self):
             pygame.draw.rect(game_window, apple.fruit_color,
                              pygame.Rect(apple.fruit_position[0], apple.fruit_position[1], 10, 10))
-
-    global color_
 
     class Snake:
         snake_body = []
@@ -135,7 +157,7 @@ def start_the_game():
         def __init__(self):
             self.snake_body = [[100, 50]]
             self.snake_position = [100, 100]
-            self.color = color_
+            self.color = globals.color_
             self.score = 0
             self.turns = 'RIGHT'
             self.direction = 'RIGHT'
@@ -154,7 +176,6 @@ def start_the_game():
                         self.turns = 'LEFT'
                     if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                         self.turns = 'RIGHT'
-
             # escape from the situation then the "simular" buttons are pushed
             if self.turns == 'UP' and self.direction != 'DOWN':
                 self.direction = 'UP'
@@ -170,11 +191,9 @@ def start_the_game():
             if k == 1:
                 my_font = pygame.font.SysFont('Times', 20)
                 game_over_surface = my_font.render('You have eaten apple and reached new level and gain +{} score and '
-                                                   'speed'.format(k),
-                                                   True,
-                                                   red)
+                                                   'speed'.format(k), True, globals.globals.red)
                 game_over_rect = game_over_surface.get_rect()
-                game_over_rect.midtop = (window_x / 2, window_y / 4)
+                game_over_rect.midtop = (globals.window_x / 2, globals.window_y / 4)
                 game_window.blit(game_over_surface, game_over_rect)
                 pygame.display.flip()
                 time.sleep(1)
@@ -182,11 +201,10 @@ def start_the_game():
             else:
                 my_font = pygame.font.SysFont('Times', 20)
                 game_over_surface = my_font.render(
-                    'You have eaten pineapple and reached new level and gain +{} score and speed'.format(k),
-                    True,
-                    red)
+                    'You have eaten pineapple and reached new level and gain +{} score and speed'.format(k), True,
+                    globals.globals.red)
                 game_over_rect = game_over_surface.get_rect()
-                game_over_rect.midtop = (window_x / 2, window_y / 4)
+                game_over_rect.midtop = (globals.window_x / 2, globals.window_y / 4)
                 game_window.blit(game_over_surface, game_over_rect)
                 pygame.display.flip()
                 time.sleep(1)
@@ -223,58 +241,36 @@ def start_the_game():
 
         # if snake touches boards
         def boards_death(self):
-            if self.snake_position[0] < 0 or self.snake_position[0] > window_x - 10:
+            if self.snake_position[0] < 0 or self.snake_position[0] > globals.window_x - 10:
                 game_over(self.score)
-            if self.snake_position[1] < 0 or self.snake_position[1] > window_y - 10:
+                # quit()
+            if self.snake_position[1] < 0 or self.snake_position[1] > globals.window_y - 10:
                 game_over(self.score)
 
         # main snake movement
         def movement(self, fruit, pineapple, snake_speed):
             while True:
-                # handling key events
-                self.change_pos()
-                # Moving the snake
-                self.current_move()
-                # Snake body growing mechanism
-                self.snake_body.insert(0, list(self.snake_position))
-                # eating fruit
+                self.change_pos(), self.current_move(), self.snake_body.insert(0, list(self.snake_position))
                 self.eat_apple()
                 if not apple.fruit_spawn:
                     apple.fruit_appear()
-
                 if not pineapple.fruit_spawn:
                     pineapple.fruit_appear()
-
-                # if user has enough score - get bonuses
                 if self.score % 5 == 0 and self.score > 0:
                     if not apple.fruit_spawn:
                         self.new_level(1)
                     else:
                         self.new_level(2)
-                    snake_speed += 1
-                    pineapple.count_pineapple += 1
-                apple.fruit_spawn = True
-                pineapple.fruit_spawn = True
-                game_window.fill(window_color)
+                    snake_speed, pineapple.count_pineapple = snake_speed + 1, pineapple.count_pineapple + 1
+                apple.fruit_spawn, pineapple.fruit_spawn = True, True
+                game_window.fill(globals.window_color)
                 for pos in self.snake_body:
-                    pygame.draw.rect(game_window, color_, pygame.Rect(pos[0], pos[1], 10, 10))
-                # new fruit
+                    pygame.draw.rect(game_window, globals.color_, pygame.Rect(pos[0], pos[1], 10, 10))
                 apple.fruit_window_appear()
                 if pineapple.count_pineapple % 2 == 0:
                     pineapple.fruit_window_appear()
-                # game over conditions
-                self.boards_death()
-                # touching the snake body
-                self.self_death()
-
-                # displaying score
-                show_score(1, white, 'Times', 20, self.score)
-
-                # Refresh game screen
-                pygame.display.update()
-
-                # Frame Per Second /Refresh Rate
-                fps.tick(snake_speed)
+                self.boards_death(), self.self_death(), show_score(1, globals.white, 'Times', 20, self.score)
+                pygame.display.update(), fps.tick(snake_speed)
 
     snake = Snake()
     apple = Apple()
@@ -284,11 +280,20 @@ def start_the_game():
     snake.movement(apple, pineapple, snake_speed)
 
 
-menu = pygame_menu.Menu('Welcome to Snake Game', 720, 480,
-                        theme=pygame_menu.themes.THEME_BLUE)
-menu.add.selector('Speed :', [('Hard', 15), ('Easy', 10)], onchange=set_difficulty)
-menu.add.selector('Snake Color :', [('Green', green), ('Red', red)], onchange=set_color)
-menu.add.selector('Window Color :', [('Black', black), ('Yellow', yellow)], onchange=set_window)
-menu.add.button('Play', start_the_game)
-menu.add.button('Quit', pygame_menu.events.EXIT)
-menu.mainloop(game_window)
+def menu_r():
+    menu = pygame_menu.Menu('Welcome to Snake Game', 720, 480,
+                            theme=pygame_menu.themes.THEME_BLUE)
+    menu.add.selector('Speed :',
+                      [('Absolutely Hard', 20), ('Hard', 15), ('Medium', 10), ('Easy', 5), ('Elementary', 3)],
+                      onchange=set_difficulty)
+    menu.add.selector('Snake Color :', [('globals.green', globals.green), ('globals.globals.red', globals.red)],
+                      onchange=set_color)
+    menu.add.selector('Window Color :', [('globals.black', globals.black), ('globals.yellow', globals.yellow)],
+                      onchange=set_window)
+    menu.add.button('Play', start_the_game)
+    menu.add.button('Rating', show_rating)
+    menu.add.button('Quit', pygame_menu.events.EXIT)
+    menu.mainloop(game_window)
+
+
+menu_r()
